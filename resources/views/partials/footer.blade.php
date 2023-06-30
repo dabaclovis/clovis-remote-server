a5310a59b2503a934de61ff949e88ee530db02df
clovis daba <dabaclovis@yahoo.com>
2023-06-30 02:56:03 +0000

    more progress

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,21 @@
+<div class=" bg-dark text-white">
+    <div class=" container p-1">
+            <div class=" text-center border-bottom">
+                <a href="" class=" btn btn-info">About</a>
+                <a href="" class=" btn btn-info"><i class="fa fa-github w3-large" aria-hidden="true"></i></a>
+                <a href="" class=" btn btn-info">D.Hub</a>
+                <a href="" class=" btn btn-info">Policy</a>
+            </div>
+            <div class=" text-center mt-1">
+               <div>All Right Reserve &copy;  <?php
+               $time = date_default_timezone_set('America/Chicago');
+              
+                   echo date('Y')
+                ?></div>
+                <div>last updated on  <?php
+                   echo date('l, M /d /Y')
+                ?></div>
+            </div>
+    </div>
+
+</div>
---
+++ resources/views/partials/modal.blade.php
@@ -0,0 +1,32 @@
+ <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Create a blog Post</h3>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
+            <form action="{{ route('articles.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="title" class=" w3-large">Title</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="title" class=" w3-large">Content</label>
+                    <textarea name="body" id="" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="file" name="image" id="image" class="form-control m-1">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">create</button>
+            </div>
+            </form>
+        </div>
+    </div>
+   </div>
